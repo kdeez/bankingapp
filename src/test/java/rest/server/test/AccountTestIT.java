b0b2bc11a39c452f7a1b3c12c4f7d659e2a940a8
eric.olaveson <eric.olaveson@servicenow.com>
2014-11-22 13:34:34 -0800

    Adding transfer funds test

--- src/test/java/rest/server/test/AccountTestIT.java
+++ src/test/java/rest/server/test/AccountTestIT.java
@@ -90,8 +90,55 @@ public class AccountTestIT {
 	}
 	
 	@Test
-	public void createAccount() throws Exception {
+	public void transferFundsBetweenUsers() throws Exception {
 		
+		HttpClient client = HttpClients.createDefault();
+		HttpClientContext context = HttpClientContext.create();
+		
+		// login admin
+		loginUser(new User("admin","password"),client,context);
+		
+		// add account1 & deposit $30
+		Account account1 = addAccount("test description", "0", client, context);
+		account1 = depositToAccount(account1, 30.00, client);
+		
+		// add account2 & deposit $30
+		Account account2 = addAccount("test description", "0", client, context);
+		account2 = depositToAccount(account2, 30.00, client);
+		
+		// might need to use same client but this seems to work too...
+		HttpClient client2 = HttpClients.createDefault();
+		HttpClientContext context2 = HttpClientContext.create();
+		
+		// transfer funds
+		HttpPost request = new HttpPost("http://localhost:8081/rest/account/transfer?id="+account2.getAccountNumber());
+		request.setHeader("Content-Type", "application/json");
+		
+		// TODO : play with the string entity object - this is what tells the request what it is asking the servlet to do
+//		String entity = "{\"accountId\":\""+account2.getAccountNumber()+"\",\"transactionType\":\"1\",\"amount\":\"5.00\",\"description\":\"test transfer\"}";
+		String entity = "{\"transactionType\":\"1\",\"accountId\":\"" + account1.getAccountNumber() + "\",\"description\":\"dinner\",\"amount\":\"5.00\"}";
+		
+		// set request entity
+		request.setEntity(new StringEntity(entity));
+		HttpResponse response = client2.execute(request, context2);
+		
+		// TODO: get this assertion to pass
+//		assertTrue("Should have recieved a HTTP 200 response", response.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
+		
+		logger.info("RESPONSE: "+response.getStatusLine().getStatusCode());
+		
+		account1 = accDao.getAccount(account1.getAccountNumber());
+		account2 = accDao.getAccount(account2.getAccountNumber());
+
+		// output - final account balances, the transfer should have occured
+		logger.info("TRANSFER ACCOUNT1: "+account1.getBalance());
+		logger.info("TRANSFER ACCOUNT2: "+account2.getBalance());
+		
+		// TODO: turn the above output statements into assertions which appropriately pass
+	}
+	
+	@Test
+	public void createAccount() throws Exception {
 		HttpClient client = HttpClients.createDefault();
 		HttpClientContext context = HttpClientContext.create();
 		
@@ -120,7 +167,6 @@ public class AccountTestIT {
 		
 		// add account
 		Account account = addAccount("description", "0", client, context);
-		System.out.println("Activity: "+ account.isActive());
 		assertTrue("description".equals(account.getDescription()));
 		assertTrue("Account should be active", account.isActive());
 		
@@ -136,7 +182,6 @@ public class AccountTestIT {
 	
 	@Test
 	public void depositAccount() throws Exception {
-		
 		HttpClient client = HttpClients.createDefault();
 		HttpClientContext context = HttpClientContext.create();
 		
@@ -159,12 +204,11 @@ public class AccountTestIT {
 		assertTrue("Should have recieved a HTTP 200 response", response.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
 		account = accDao.getAccount(account.getAccountNumber());
 		double endingBalance = account.getBalance();
-		assertTrue("", endingBalance - startingBalance == 30.0);
+		assertTrue("30 dollars should have been added.", endingBalance - startingBalance == 30.0);
 	}
 	
 	@Test
 	public void debitAccount() throws Exception {
-		
 		HttpClient client = HttpClients.createDefault();
 		HttpClientContext context = HttpClientContext.create();
 		
@@ -172,9 +216,28 @@ public class AccountTestIT {
 		loginUser(new User("admin","password"),client,context);
 		
 		// add account
-		Account account = addAccount("description delete", "0", client, context);
+		Account account = addAccount("test description", "0", client, context);
 		
-		// configure deposit request
+		// add funds
+		account = depositToAccount(account, 30.00, client);
+		
+		// configure debit request
+		HttpPost request = new HttpPost(TRANSACTION_REQUEST);
+		request.setHeader("Content-Type", "application/json");
+		String entity = "{\"accountId\":\""+account.getAccountNumber()+"\",\"transactionType\":\"0\",\"amount\":\"5.00\",\"description\":\"test debit\"}";
+		request.setEntity(new StringEntity(entity));
+		
+		// execute request noting balance
+		double startingBalance = account.getBalance();
+		HttpResponse response = client.execute(request);
+		
+		assertTrue("Should have recieved a HTTP 200 response", response.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
+		account = accDao.getAccount(account.getAccountNumber());
+		double endingBalance = (int)account.getBalance();
+		assertTrue("5 dollars should have been debited.", startingBalance - endingBalance == 5.0);
+	}
+	
+	public Account depositToAccount(Account account, double amount, HttpClient client) throws Exception {
 		HttpPost request = new HttpPost(TRANSACTION_REQUEST);
 		request.setHeader("Content-Type", "application/json");
 		String entity = "{\"accountId\":\""+account.getAccountNumber()+"\",\"transactionType\":\"1\",\"amount\":\"30.00\",\"description\":\"test deposit\"}";
@@ -186,22 +249,10 @@ public class AccountTestIT {
 		
 		assertTrue("Should have recieved a HTTP 200 response", response.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
 		account = accDao.getAccount(account.getAccountNumber());
+		double endingBalance = account.getBalance();
+		assertTrue("30 doll hairs should have been added.", endingBalance - startingBalance == 30.0);
 		
-		// configure debit request
-		request = new HttpPost(TRANSACTION_REQUEST);
-		request.setHeader("Content-Type", "application/json");
-		entity = "{\"accountId\":\""+account.getAccountNumber()+"\",\"transactionType\":\"0\",\"amount\":\"5.00\",\"description\":\"test debit\"}";
-		System.out.println("ENTITY: "+entity);
-		request.setEntity(new StringEntity(entity));
-		
-		// execute request noting balance
-		startingBalance = account.getBalance();
-		response = client.execute(request);
-		
-		assertTrue("Should have recieved a HTTP 200 response", response.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
-		account = accDao.getAccount(account.getAccountNumber());
-		double endingBalance = (int)account.getBalance();
-		assertTrue("", startingBalance - endingBalance == 5.0);
+		return account;
 	}
 	
 	/**
@@ -217,7 +268,7 @@ public class AccountTestIT {
 		HttpPost accountRequest = new HttpPost(ACCOUNT_REQUEST);
 		accountRequest.setHeader("Content-Type", "application/json");
 		accountRequest.setEntity(new StringEntity("{\"description\":\"" + description + "\",\"accountType\":\""+ type +"\"}"));
-		HttpResponse response2 = client.execute(accountRequest);
+		HttpResponse response2 = client.execute(accountRequest, context);
 		
 		// test account persisted ok
 		assertTrue("Should have recieved a HTTP 200 response", response2.getStatusLine().getStatusCode() == Status.OK.getStatusCode());
